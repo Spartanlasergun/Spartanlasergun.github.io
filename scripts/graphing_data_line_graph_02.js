a8220140b6025d403c52c270701157bda19a5986
Spartanlasergun <bns360@live.com>
2023-06-24 19:36:57 -0400

    updated content

---
+++ scripts/graphing_data_line_graph_02.js
@@ -0,0 +1,21 @@
+const line_one = document.getElementById('profits');
+
+
+
+const data_one = {
+  labels: ["2010", "2012", "2014", "2016", "2018"],
+  datasets: [{
+    label: 'Profits Over Time',
+    data: [65000, 59000, 80000, 81000, 56000],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+
+
+new Chart(line_one, {
+  type: 'line',
+  data: data_one,
+});
+
