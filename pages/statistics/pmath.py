dae98e352d02bb9a6da7cabecc98ded5c773e7fc
Spartanlasergun <spartanlasergun@outlook.com>
2023-09-18 09:27:51 -0400

    updated content

--- pages/statistics/pmath.py
+++ pages/statistics/pmath.py
@@ -1,33 +1,29 @@
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
 import numpy as np
 
-a = [150.5, 151.5, 152.5, 153.5, 154.5]
-b = [5, 13, 28, 31, 37]
+bins = [150, 153, 155, 156, 158, 160]
+mid_points = [151.5, 154, 155.5, 157, 159]
+freq = [5, 8, 15, 3, 6]
+raw_data = []
+index = 0
+for item in freq:
+	count = item
+	while count != 0:
+		raw_data.append(mid_points[index])
+		count = count - 1
+	index = index + 1
 
-x = np.linspace(150.5, 154.5, 100)
-func = interp1d(a, b, kind='quadratic')
-y = func(x)
+freq_den = []
+index = 0
+widths = [3, 2, 1, 2, 2]
+for item in freq:
+	density = item / widths[index]
+	freq_den.append(density)
+	index = index + 1
+print(freq_den)
 
-x_list = x.tolist()
-y_list = y.tolist()
+#plt.title('Histogram')
+#plt.hist(raw_data, bins=bins, density=True, edgecolor='black')
+#plt.show()
 
-for item in y_list:
-	if item <= (37/2):
-		index = y_list.index(item)
-median = x_list[index]
-freq_mid = 37/2
-
-medx = [150, median, median, median]
-medy = [freq_mid, freq_mid, freq_mid, 0]
-
-tex = "(" + str(round(median, 3)) + ", " + str(freq_mid) + ")" 
-
-plt.grid(True)
-plt.title("Cumulative Frequency Curve")
-plt.scatter(a, b)
-plt.plot(medx, medy, linestyle='--')
-plt.plot(x, y)
-# Annotate a specific point (e.g., the point at index 2)
-plt.text(median, freq_mid, tex, fontsize=12, ha='center', va='bottom')
-plt.savefig('cum_freq_curve.png', transparent=True, dpi=75)
+#plt.savefig('histogram.png', transparent=True, dpi=90)
